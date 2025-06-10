ef760d26239f2283415c968499dad0650c84eb40
Bang-Shiuh Chen <bangshiuh.chen@gmail.com>
2025-06-10 10:29:01 -0400

    [data] update helium plasma mechanism

--- extensible_two_temp_plasma.py
+++ extensible_two_temp_plasma.py
@@ -1,4 +1,38 @@
 import cantera as ct
+from math import exp
+
+class ExtensibleArrheniusData(ct.ExtensibleRateData):
+    __slots__ = ("T",)
+    def __init__(self):
+        self.T = None
+
+    def update(self, gas):
+        T = gas.T
+        if self.T != T:
+            self.T = T
+            return True
+        else:
+            return False
+
+@ct.extension(name="extensible-Arrhenius", data=ExtensibleArrheniusData)
+class ExtensibleArrhenius(ct.ExtensibleRate):
+    __slots__ = ("A", "b", "Ea_R")
+    def set_parameters(self, params, units):
+        self.A = params.convert_rate_coeff("A", units)
+        self.b = params["b"]
+        self.Ea_R = params.convert_activation_energy("Ea", "K")
+
+    def get_parameters(self, params):
+        params.set_quantity("A", self.A, self.conversion_units)
+        params["b"] = self.b
+        params.set_activation_energy("Ea", self.Ea_R, "K")
+
+    def validate(self, equation, soln):
+        if self.A < 0:
+            raise ValueError(f"Found negative 'A' for reaction {equation}")
+
+    def eval(self, data):
+        return self.A * (data.T / 300)**self.b * exp(-self.Ea_R/data.T)
 
 class ExtensibleTwoTempPlasmaData(ct.ExtensibleRateData):
     __slots__ = ("T", "Te")
@@ -40,6 +74,3 @@ class TwoTempPlasmaRate(ct.ExtensibleRate):
         # rate = A * (Te / Tgas)^b
         return self.A * (data.Te / data.T) ** self.b
 
-
-plasma = ct.Solution("data/helium-oxygen-hydrogen-plasma.yaml")
-print(plasma.Te)
